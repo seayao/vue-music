5a0016f0f338143da20fba4cd9b0132d6db5a1e4
barnboo <250159440@qq.com>
2017-08-29 17:03:47 +0800

    first commit

---
+++ src/components/m-header/m-header.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="m-header">
+    <div class="icon"></div>
+    <router-link tag="div" class="mine" to="/user">
+      <i class="icon-mine"></i>
+    </router-link>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "~common/stylus/variable"
+  @import "~common/stylus/mixin"
+
+  .m-header
+    position: relative
+    height: 44px
+    color: $color-theme
+    font-size: 0
+    background:$color-header
+    .icon
+      display: inline-block
+      vertical-align: top
+      margin-top: 3px
+      margin-left: 12px
+      width: 150px
+      height: 36px
+      bg-image('logo')
+      background-size: cover
+    .mine
+      position: absolute
+      top: 0
+      right: 0
+      .icon-mine
+        display: block
+        padding: 12px
+        font-size: 20px
+        color: $color-text
+</style>
